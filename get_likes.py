cd688451d6ea3c490ae0a61f7faa4f8144abee9d
Joe Mason <joe@notcharles.ca>
2022-11-05 21:09:32 -0400

    Fix up tweets with user and media details

--- get_likes.py
+++ get_likes.py
@@ -3,58 +3,84 @@
 # found in the LICENSE file.
 
 import creds
+import pprint
 import tweepy
 
-client = tweepy.Client(bearer_token=creds.bearer_token, consumer_key=creds.consumer_key,
-                       consumer_secret=creds.consumer_secret, access_token=creds.access_token, access_token_secret=creds.access_token_secret)
+EXPANSIONS = [
+    'author_id',
+    'referenced_tweets.id',
+    'in_reply_to_user_id',
+    'attachments.media_keys',
+]
+
+MEDIA_FIELDS = [
+    'media_key',
+    'type',
+    'url',
+    'duration_ms',
+    'height',
+    'width',
+    'preview_image_url',
+    'alt_text',
+    'variants',
+]
+
+TWEET_FIELDS = [
+    'id',
+    'text',
+    'attachments',
+    'author_id',
+    'created_at',
+    'lang',
+    'referenced_tweets',
+]
+
+client = tweepy.Client(bearer_token=creds.bearer_token,
+                       consumer_key=creds.consumer_key,
+                       consumer_secret=creds.consumer_secret,
+                       access_token=creds.access_token,
+                       access_token_secret=creds.access_token_secret,
+                       return_type=dict)
+
+
+def _get_member(d, key, default):
+    return key in d and d[key] or default
 
 
 def get_likes():
-    expansions = [
-        'author_id',
-        'referenced_tweets.id',
-        'in_reply_to_user_id',
-        'attachments.media_keys',
-        'attachments.poll_ids',
-        'entities.mentions.username',
-        'referenced_tweets.id.author_id',
-    ]
-    media_fields = [
-        'media_key',
-        'type',
-        'url',
-        'duration_ms',
-        'height',
-        'width',
-        'preview_image_url',
-        'alt_text',
-        'variants',
-    ]
-    tweet_fields = [
-        'id',
-        'text',
-        'attachments',
-        'author_id',
-        'conversation_id',
-        'created_at',
-        'entities',
-        'in_reply_to_user_id',
-        'lang',
-        'referenced_tweets',
-    ]
     liked_tweets = {}
+    users = {}
+    media = {}
     next_token = None
     while True:
-        response = client.get_liked_tweets(
-            creds.user_id, pagination_token=next_token, expansions=expansions)
-        if response.data:
-            for tweet in response.data:
-                liked_tweets[tweet.id] = tweet
+        response = client.get_liked_tweets(creds.user_id,
+                                           pagination_token=next_token,
+                                           expansions=EXPANSIONS,
+                                           media_fields=MEDIA_FIELDS,
+                                           tweet_fields=TWEET_FIELDS)
+        pprint.pp(response)
+        for tweet in _get_member(response, 'data', []):
+            liked_tweets[tweet['id']] = tweet
+        includes = _get_member(response, 'includes', {})
+        for m in _get_member(includes, 'media', []):
+            media[m['media_key']] = m
+        for u in _get_member(includes, 'users', []):
+            users[u['id']] = u
         # TODO: go through response.includes, get media info, etc.
         # tweet.data includes attachments/media_keys which should map the media info to tweets. (Same for author_id)
         # Also handle response.errors
         try:
-            next_token = response.meta['next_token']
+            next_token = _get_member(response, 'meta', {})['next_token']
         except KeyError:
             break
+
+    # Attach users and media to tweets
+    for tweet in liked_tweets.values():
+        if 'author_id' in tweet:
+            tweet['author'] = _get_member(users, tweet['author_id'], None)
+        attachments = _get_member(tweet, 'attachments', {})
+        tweet['media'] = []
+        for media_key in _get_member(attachments, 'media_keys', []):
+            tweet['media'].append(_get_member(media, media_key, None))
+
     return liked_tweets
